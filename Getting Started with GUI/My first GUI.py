78b39133650b181a30e241b07e43f3267a2653ea
Shreyasi Mandal <shreyasi18sia@gmail.com>
2021-06-03 20:03:41 +0530

    A few modifications

--- Getting Started with GUI/My first GUI.py
+++ Getting Started with GUI/My first GUI.py
@@ -1,18 +1,18 @@
-from tkinter import Tk, Label, Button
+from tkinter import *
 
 class MyFirstGUI:
     def __init__(self, master):
         self.master = master
         master.title("A Simple GUI!")
 
-        self.label = Label(master, text = "This is our first GUI!")
-        self.label.pack()
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.grid(columnspan=2, sticky=W)
 
-        self.greet_button = Button(master, text = "Greet", command = self.greet)
-        self.greet_button.pack()
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.grid(row=1)
 
-        self.close_button = Button(master, text = "Close", command = master.quit)
-        self.close_button.pack()
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.grid(row=1, column=1)
 
     def greet(self):
         print("Greetings!")
